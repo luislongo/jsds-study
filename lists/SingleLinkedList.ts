05f1c97f80fb35cca1868963c097458dcb1266dd
luislongo <luis.longof@gmail.com>
2022-01-19 09:34:38 -0300

    Added SingleLinkedList and tests

---
+++ lists/SingleLinkedList.ts
@@ -0,0 +1,130 @@
+export default class SingleLinkedList<T> implements ISingleLinkedList<T> {
+    #head: Node<T> | undefined;
+    #length: number;
+
+    constructor() {
+        this.#head = undefined;
+        this.#length = 0;
+    }
+
+    length(): number {
+        return this.#length;
+    }
+
+    head(): T | undefined {
+        return this.#head ? this.#head.value : undefined;
+    }
+
+    tail(): T | undefined {
+        // Empty list
+        if (this.#length == 0) return undefined;
+
+        // General case
+        let pivot = this.#head!;
+        while (pivot?.hasNext()) {
+            pivot = pivot.next()!;
+        }
+
+        return pivot.value;
+    }
+
+    push(value: T): void {
+        const node = new Node(value);
+
+        // Empty list
+        if (this.#length == 0) {
+            this.#length += 1;
+            this.#head = node;
+            return;
+        }
+
+        // General case
+        let pivot = this.#head!;
+        while (pivot.hasNext()) {
+            pivot = pivot.next()!;
+        }
+        this.#length += 1;
+        pivot.setNext(node);
+    }
+
+    pop(): T | undefined {
+        // Empty list
+        if (this.#length == 0) {
+            return undefined;
+        }
+
+        this.#length -= 1;
+        // Single node list
+        let pivot = this.#head!;
+        if (!pivot.hasNext()) {
+            this.#head = undefined;
+            return pivot.value;
+        }
+
+        // General case
+        while (pivot.next()!.hasNext()) {
+            pivot = pivot.next()!;
+        }
+        const node = pivot.next()!;
+        pivot.setNext(undefined);
+        return node.value;
+    }
+
+    shift(): T | undefined {
+        // Empty list
+        if (this.#length == 0) {
+            return undefined;
+        }
+
+        // General case
+        this.#length -= 1;
+        let pivot = this.#head!;
+        this.#head = pivot.next();
+        return pivot.value;
+    }
+
+    unshift(value: T): void {
+        const node = new Node(value);
+
+        // Empty list
+        if (this.#length == 0) {
+            this.#length += 1;
+            this.#head = node;
+            return;
+        }
+
+        // General case
+        node.setNext(this.#head);
+        this.#head = node;
+        this.#length += 1;
+    }
+}
+
+export interface ISingleLinkedList<T> {
+    length(): number; // O(1)
+    head(): T | undefined; // O(1)
+    tail(): T | undefined; // O(n)
+    push(value: T): void; // O(n)
+    pop(): T | undefined; // O(n)
+    shift(): T | undefined; // O(1)
+    unshift(value: T): void; // O(1)
+}
+
+class Node<T> {
+    #next: Node<T> | undefined;
+    value: T;
+
+    constructor(value: T) {
+        this.#next = undefined;
+        this.value = value;
+    }
+    next(): Node<T> | undefined {
+        return this.#next;
+    }
+    setNext(next: Node<T> | undefined): void {
+        this.#next = next;
+    }
+    hasNext(): boolean {
+        return this.#next != undefined;
+    }
+}
---
+++ test/SingleLinkedList.test.ts
@@ -0,0 +1,210 @@
+import SingleLinkedList from '../lists/SingleLinkedList';
+
+test('Newly created list should be empty', () => {
+    let sll = new SingleLinkedList();
+
+    expect(sll.length()).toBe(0);
+});
+test('Head should be undefined in an empty list', () => {
+    let sll = new SingleLinkedList();
+
+    expect(sll.head()).toBe(undefined);
+});
+test('First push should update head', () => {
+    let sll = new SingleLinkedList<string>();
+    sll.push('Head');
+
+    expect(sll.head()).toBe('Head');
+});
+test('Each push should increase length by 1', () => {
+    let sll = new SingleLinkedList<string>();
+
+    sll.push('Head');
+    expect(sll.length()).toBe(1);
+
+    sll.push('Middle');
+    expect(sll.length()).toBe(2);
+
+    sll.push('Tail');
+    expect(sll.length()).toBe(3);
+});
+test('Head should be the same after pushes', () => {
+    let sll = new SingleLinkedList<string>();
+
+    sll.push('Head');
+    expect(sll.head()).toBe('Head');
+
+    sll.push('Middle');
+    expect(sll.head()).toBe('Head');
+
+    sll.push('Tail');
+    expect(sll.head()).toBe('Head');
+});
+test('Tail should be updated after each push', () => {
+    let sll = new SingleLinkedList<string>();
+
+    sll.push('Head');
+    expect(sll.tail()).toBe('Head');
+
+    sll.push('Middle');
+    expect(sll.tail()).toBe('Middle');
+
+    sll.push('Tail');
+    expect(sll.tail()).toBe('Tail');
+});
+test('Popping an empty list should return undefined', () => {
+    let sll = new SingleLinkedList<string>();
+    expect(sll.pop()).toBe(undefined);
+});
+test('Popping an empty list should not decrease length', () => {
+    let sll = new SingleLinkedList<string>();
+
+    sll.pop();
+    expect(sll.length()).toBe(0);
+});
+test('Each pop should return the last element inserted', () => {
+    let sll = new SingleLinkedList<string>();
+    sll.push('Head');
+    sll.push('Middle');
+    sll.push('Tail');
+
+    expect(sll.pop()).toBe('Tail');
+    expect(sll.pop()).toBe('Middle');
+    expect(sll.pop()).toBe('Head');
+});
+test('Popping an one element list should update head to undefined', () => {
+    let sll = new SingleLinkedList<string>();
+    sll.push('Head');
+    sll.pop();
+
+    expect(sll.head()).toBe(undefined);
+});
+test('Popping elements should update tail', () => {
+    let sll = new SingleLinkedList<string>();
+    sll.push('Head');
+    sll.push('Middle');
+    sll.push('Tail');
+
+    expect(sll.tail()).toBe('Tail');
+
+    sll.pop();
+    expect(sll.tail()).toBe('Middle');
+
+    sll.pop();
+    expect(sll.tail()).toBe('Head');
+});
+test('Each pop should decrease length by 1', () => {
+    let sll = new SingleLinkedList<string>();
+    sll.push('Head');
+    sll.push('Middle');
+    sll.push('Tail');
+
+    sll.pop();
+    expect(sll.length()).toBe(2);
+
+    sll.pop();
+    expect(sll.length()).toBe(1);
+
+    sll.pop();
+    expect(sll.length()).toBe(0);
+});
+
+test('Shifting an empty list should return undefined', () => {
+    let sll = new SingleLinkedList<string>();
+    expect(sll.shift()).toBe(undefined);
+});
+test('Shifting an empty list should not decrease length', () => {
+    let sll = new SingleLinkedList<string>();
+
+    sll.shift();
+    expect(sll.length()).toBe(0);
+});
+test('Each shift should return the first element in the list', () => {
+    let sll = new SingleLinkedList<string>();
+    sll.push('Head');
+    sll.push('Middle');
+    sll.push('Tail');
+
+    expect(sll.shift()).toBe('Head');
+    expect(sll.shift()).toBe('Middle');
+    expect(sll.shift()).toBe('Tail');
+});
+test('Shifting an one element list should update head to undefined', () => {
+    let sll = new SingleLinkedList<string>();
+    sll.push('Head');
+    sll.shift();
+
+    expect(sll.head()).toBe(undefined);
+});
+test('Shifting elements should update head', () => {
+    let sll = new SingleLinkedList<string>();
+    sll.push('Head');
+    sll.push('Middle');
+    sll.push('Tail');
+
+    expect(sll.head()).toBe('Head');
+
+    sll.shift();
+    expect(sll.head()).toBe('Middle');
+
+    sll.shift();
+    expect(sll.head()).toBe('Tail');
+});
+test('Each shift should decrease length by 1', () => {
+    let sll = new SingleLinkedList<string>();
+    sll.push('Head');
+    sll.push('Middle');
+    sll.push('Tail');
+
+    sll.shift();
+    expect(sll.length()).toBe(2);
+
+    sll.shift();
+    expect(sll.length()).toBe(1);
+
+    sll.shift();
+    expect(sll.length()).toBe(0);
+});
+
+test('First unshift should update head', () => {
+    let sll = new SingleLinkedList<string>();
+    sll.unshift('Head');
+
+    expect(sll.head()).toBe('Head');
+});
+test('Each unshift should increase length by 1', () => {
+    let sll = new SingleLinkedList<string>();
+
+    sll.push('Tail');
+    expect(sll.length()).toBe(1);
+
+    sll.push('Middle');
+    expect(sll.length()).toBe(2);
+
+    sll.push('Head');
+    expect(sll.length()).toBe(3);
+});
+test('Tail should be the same after unshifts', () => {
+    let sll = new SingleLinkedList<string>();
+
+    sll.unshift('Tail');
+    expect(sll.tail()).toBe('Tail');
+
+    sll.unshift('Middle');
+    expect(sll.tail()).toBe('Tail');
+
+    sll.unshift('Head');
+    expect(sll.tail()).toBe('Tail');
+});
+test('Head should be updated after each unshift', () => {
+    let sll = new SingleLinkedList<string>();
+
+    sll.unshift('Tail');
+    expect(sll.head()).toBe('Tail');
+
+    sll.unshift('Middle');
+    expect(sll.head()).toBe('Middle');
+
+    sll.unshift('Head');
+    expect(sll.head()).toBe('Head');
+});
